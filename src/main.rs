b905ee81f4aaed0dffb9136b5e0f03c1000536fd
DATADEER <konto@datadeer.de>
2024-08-30 00:19:53 +0200

    * read the wordlist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{fs::File};
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    FileLoader::load_wordlist();
 }
+
+struct FileLoader;
+
+impl FileLoader {
+    fn load_wordlist(){
+
+        let file_result = File::open("./src/wordlist.txt");
+
+        if let Ok(wordlist) = file_result {
+            let reader = BufReader::new(wordlist);
+
+            for line in reader.lines() {
+                println!("{:?}", line.unwrap_or("".to_string()).trim().to_string());
+            }
+
+        } else if let Err(error) = file_result {
+            println!("{:?}", error);
+        }
+    }
+}
+
