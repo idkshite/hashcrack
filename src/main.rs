87a0d539e2d741e64c442e8aac5c003d2b5930cf
DATADEER <konto@datadeer.de>
2024-09-01 17:56:41 +0200

    * read hash_to_crack through params

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
-use std::{fs::File};
+use std::{env, fs::File};
+use std::error::Error;
 use std::io::{BufReader, BufRead};
 
-fn main() {
-    FileLoader::load_wordlist();
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let hash_to_crack = args.get(1).expect("No hash provided. Provide hash as first parameter");
+
+
+
+
+    match FileLoader::load_wordlist() {
+        Ok(words) => println!("Loaded {} words", words.len()),
+        Err(e) => {
+            eprintln!("Error loading wordlist: {}", e);
+            // We can get more details about the error:
+            eprintln!("Error kind: {:?}", e.kind());
+        }
+    }
+
+    Ok(())
 }
 
 struct FileLoader;
 
 impl FileLoader {
-    fn load_wordlist(){
+    fn load_wordlist() -> Result<Vec<String>, std::io::Error>{
 
+        // this could be written with ? and without if let
         let file_result = File::open("./src/wordlist.txt");
 
-        if let Ok(wordlist) = file_result {
-            let reader = BufReader::new(wordlist);
+        match file_result {
+            Ok(wordlist) => {
+                let reader = BufReader::new(wordlist);
 
-            for line in reader.lines() {
-                println!("{:?}", line.unwrap_or("".to_string()).trim().to_string());
+                // reader.lines().map(|line_result| line_result.map(|line| line.to_string().trim()))
+                reader.lines()
+                    .map(|line_result| line_result.map(|l| l.trim().to_string()))
+                    .collect()
+            },
+            Err(error) => {
+                println!("{:?}", error);
+                Err(error)
             }
-
-        } else if let Err(error) = file_result {
-            println!("{:?}", error);
         }
     }
 }
